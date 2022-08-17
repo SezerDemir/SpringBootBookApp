2d6246c73e2c3f62e739d751fb2a45704d7721e1
Sezer Demir <sezer_d@outlook.com>
2022-08-17 10:47:58 +0300

    QUERY annotation is tried with JPQL

--- BookRestApp/src/main/java/com/sezer/book/BookRepo.java
+++ BookRestApp/src/main/java/com/sezer/book/BookRepo.java
@@ -3,11 +3,13 @@ package com.sezer.book;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookRepo extends JpaRepository<Book, Integer>{
-
 	
-	Optional<Book> findByName(String name);
+	@Query("SELECT b FROM Book b WHERE b.name= :name")
+	Optional<Book> findByName(@Param("name") String name);
 }
