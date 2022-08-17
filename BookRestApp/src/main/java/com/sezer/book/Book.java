82bad3fdef85c7c9041d817461f169a9d31bc550
Sezer Demir <sezer_d@outlook.com>
2022-08-17 09:56:30 +0300

    id generation method is changed

--- BookRestApp/src/main/java/com/sezer/book/Book.java
+++ BookRestApp/src/main/java/com/sezer/book/Book.java
@@ -20,16 +20,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Book {
 	@Id
-	@SequenceGenerator(
-			name = "book_sequence",
-			sequenceName = "book_sequence",
-			allocationSize  = 1
-	)
-	@GeneratedValue(
-			strategy = GenerationType.SEQUENCE,
-			generator = "book_sequence"
-	)
-	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 	private LocalDate publishDate;
